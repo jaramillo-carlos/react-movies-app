79416997918cf7b06b1edc9926063654af6138a4
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-09 17:34:12 -0500

    fetch in saga, and update state on reducer

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = 'https://www.omdbapi.com/?apiKey=ffd0c3a5';
 
 // this function wen be called, return a promise  
-export const apiCall = (url, data, headers, methods) => axios({
+export const apiCall = (url, data, headers, method) => axios({
   method,
   url: baseURL + url,
   data,
