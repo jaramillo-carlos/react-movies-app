358909137d0724588311be219a2405b63f04ea1d
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-09 13:49:31 -0500

    create sagas to watch call actions

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,9 @@
 import { all } from 'redux-saga/effects'
+import search from './search'
 
 export default function* rootSaga() {
-  yield all([])
+  // when be executed all watchers
+  yield all([
+    search()
+  ])
 }
---
+++ src/redux/sagas/search.js
@@ -0,0 +1,21 @@
+import { put, call, takeLatest } from 'redux-saga/effects';
+import { 
+  SEARCH_MOVIE_START, 
+  SEARCH_MOVIE_ERROR, 
+  SEARCH_MOVIE_COMPLETE
+} from '../../consts/actionTypes';
+
+export function* searchMovie({payload}) {
+  try {
+
+  } catch (error) {
+
+  }
+}
+// export to put in watcher
+// and be sagas stay listen to be called
+export default function* search() {
+  // to listen SEARCH_MOVIE_START action
+  // and when called, call searchMovie Function
+  yield takeLatest(SEARCH_MOVIE_START, searchMovie);
+}
