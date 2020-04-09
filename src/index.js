37ae5ece6cc08a1d68c69d89928a3434f172c522
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-09 11:31:37 -0500

    Initial config, redux, redux-sagas, react-redux, and material ui

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './redux/store';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
