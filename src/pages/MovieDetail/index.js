38b36387e27b229dc4d60aea9abc8e8e85cf99f8
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-10 01:13:30 -0500

    fixing bugs

--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { Container } from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, CircularProgress, Typography } from '@material-ui/core'
 
-export default () => {
-  return(
+import { searchMovieById } from '../../redux/actions/search'
+import { movieResult as MovieResultSelector } from '../../redux/selectors'
+
+export default ({ match }) => {
+  const dispatch = useDispatch();
+  const movieResult = useSelector(MovieResultSelector)
+
+  useEffect(() => {
+    // const movieId = match.params.id;
+    // const { id: movieId } = match.params;
+    const { params: { id: movieId } } = match;
+    if (!movieResult || movieResult && movieResult.imdbID !== movieId) {
+      dispatch(searchMovieById({ movieId }))
+    }
+  })
+
+  if (!movieResult) {
+    return <CircularProgress size="50" color="primary" />
+  }
+  const {Title, Poster, Actors, Director, Country, Rated, Awards, Plot } = movieResult;
+  return (
     <Container>
-      Movie Detail
+      <Typography variant="h3">{Title}</Typography>
+      <img src={Poster} alt={Title} />
+      <Typography><strong>Actores: </strong> {Actors}</Typography>
+      <Typography><strong>Director: </strong> {Director}</Typography>
+      <Typography><strong>Pais: </strong> {Country}</Typography>
+      <Typography><strong>Clasificacion: </strong> {Rated}</Typography>
+      <Typography><strong>Premios: </strong> {Awards}</Typography>
+      <Typography><strong>Sinopsis: </strong> {Plot}</Typography>
     </Container>
   )
 }
--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Container, CircularProgress } from '@material-ui/core'
 import queryString from 'query-string'
@@ -13,14 +13,16 @@ export default ({location}) => {
   const movies = useSelector(state => movieResults(state));
   // state have all store, but use Selectors, to get only do i need.
   const isLoading = useSelector(isSearchLoading)
+  const [isLooked, setIsLooked] = useState(false)
 
   // useEffect get a callback to be executed on lifeCycles
   useEffect(() =>{
     const { movieName } = queryString.parse(location.search)
-    if (movieName && !movies) {
+    if (movieName && !isLooked) {
+      setIsLooked(true)
       dispatch(searchMovie({movieName}))
     }
-  })
+  },[isLooked])
 
   // this is bad idea, because will go listen all time, only need on component didMount
   // console.log(queryString.parse(location.search));  // to parse vars
