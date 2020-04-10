50a33840421296ccc4208eb5c049243741ffc4b9
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-04-09 20:48:09 -0500

    create selectors, for get specific info from store

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -3,19 +3,21 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Container } from '@material-ui/core'
 import queryString from 'query-string'
 
-import {searchMovie} from '../../redux/actions/search';
-
+import {searchMovie} from '../../redux/actions/search'
+import { movieResults, isSearchLoading } from '../../redux/selectors'
 
 export default ({location}) => {
   const dispatch = useDispatch();
-  const movies = useSelector(state => {
-    console.log(state);
-  });
-
+  const movies = useSelector(movieResults);
+  // state have all store, but use Selectors, to get only do i need.
+  // const movies = useSelector(state => movieResults(state));
+  
   // useEffect get a callback to be executed on lifeCycles
   useEffect(() =>{
     const { movieName } = queryString.parse(location.search)
-    dispatch(searchMovie({movieName}))
+    if (movieName && !movies) {
+      dispatch(searchMovie({movieName}))
+    }
   })
 
   // this is bad idea, because will go listen all time, only need on component didMount
